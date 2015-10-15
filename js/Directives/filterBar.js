29a8b50a9f4a10aa4cf1873cdfc04e2e1a95ba5d
Ken Lee <kdblitz@gmail.com>
2015-10-15 23:22:27 +0800

    Changed toolbar to filter bar for clarity.

---
+++ js/Directives/filterBar.js
@@ -0,0 +1,13 @@
+app.directive('filterBar', function() {
+  return {
+    restrict: 'E',
+    replace:true,
+    templateUrl: 'js/Directives/filterBar.html',
+    scope: {
+      filterOptions:'=',
+      products:'='
+    }, link: function(scope) {
+
+    }
+  };
+});
--- js/Directives/toolbar.js
+++
@@ -1,7 +0,0 @@
-app.directive('toolbar', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'js/Directives/toolbar.html',
-    replace:true
-  }
-});
