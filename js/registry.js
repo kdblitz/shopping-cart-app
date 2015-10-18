6514cbd16f3f77b7b7b85339530918e32eb3da27
Ken Lee <kdblitz@gmail.com>
2015-10-19 00:51:19 +0800

    Added routing.

--- js/registry.js
+++ js/registry.js
@@ -1,1 +1,14 @@
-var app = angular.module('ShoppingApp',[]);
+var app = angular.module('ShoppingApp',['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/catalog', {
+    template:'<product-list filter-options="filterOptions" products="products"></product-list>'
+  })
+  .when('/orderForm', {
+    template:'<order-form></order-form>'
+  })
+  .otherwise({
+    redirectTo: '/catalog'
+  })
+});
